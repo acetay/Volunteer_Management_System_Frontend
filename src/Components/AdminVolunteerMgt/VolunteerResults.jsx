f3aac25fba05d615f502b3521abdc4ad6d4667e7
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-03-30 22:29:00 +0800

    completed pagination

--- src/Components/AdminVolunteerMgt/VolunteerResults.jsx
+++ src/Components/AdminVolunteerMgt/VolunteerResults.jsx
@@ -16,6 +16,18 @@ function VolunteerResults() {
     language: 'na',
   });
 
+  // Pagination Logic
+  const [numOfVolPerPage, setNumOfVolPerPage] = useState(8);
+  const [page, setPage] = useState(1);
+  const lastIndex = page * numOfVolPerPage;
+  const firstIndex = lastIndex - numOfVolPerPage;
+  const volunteersShownOnPage = volunteersCopy?.slice(firstIndex, lastIndex);
+  const totalPages = Math.ceil(volunteersCopy?.length / numOfVolPerPage);
+
+  const changePage = (num) => {
+    setPage(() => num);
+  };
+
   useEffect(() => {
     setVolunteersCopy(volunteers);
   }, []);
@@ -40,6 +52,7 @@ function VolunteerResults() {
       filters.language || 'na'
     );
     setVolunteersCopy(searchByFilters);
+    setPage(() => 1);
   };
 
   const handleChange = (e) => {
@@ -72,21 +85,41 @@ function VolunteerResults() {
           />
         </div>
         <div className="btn-group flex justify-center items-center pb-4 text-white">
-          <button className="btn btn-info btn-md text-white btn-active">
+          {totalPages ? (
+            [...new Array(totalPages).fill(true)].map((page, index) => (
+              <button
+                onClick={() => changePage(index + 1)}
+                key={index}
+                className="btn btn-info btn-md text-white"
+              >
+                {index + 1}
+              </button>
+            ))
+          ) : (
+            <></>
+          )}
+
+          {/* <button className="btn btn-info btn-md text-white btn-active">
             1
           </button>
           <button className="btn btn-md btn-info text-white">2</button>
           <button className="btn btn-info btn-md text-white">3</button>
-          <button className="btn btn-info btn-md text-white">4</button>
+          <button className="btn btn-info btn-md text-white">4</button> */}
         </div>
         <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-          {volunteersCopy?.map((volunteer) => (
-            <VolunteerItem
-              key={volunteer.id}
-              volunteer={volunteer}
-              toggle={toggle}
-            />
-          ))}
+          {volunteersCopy ? (
+            volunteersShownOnPage?.map((volunteer) => (
+              <VolunteerItem
+                key={volunteer.id}
+                volunteer={volunteer}
+                toggle={toggle}
+              />
+            ))
+          ) : (
+            <div className="text-error col-span-4">
+              <p className="text-center">No search results</p>
+            </div>
+          )}
         </div>
       </>
     );
